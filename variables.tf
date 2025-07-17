552928c3e7abd41eca1d0564aed11e97b3b85b5c
Shreyy2002 <124439708+Shreyy2002@users.noreply.github.com>
2025-07-17 12:42:07 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,11 @@
-variable "aws_region" {
+variable "region" {
   type    = string
   default = "us-east-1"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
 }
 
 variable "environment" {
