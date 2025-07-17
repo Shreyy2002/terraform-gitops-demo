989819f3d7405258f230176e6d992d3fd4b06abe
Shreyy2002 <124439708+Shreyy2002@users.noreply.github.com>
2025-07-17 12:06:28 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "random_id" "id" {
+  byte_length = 4
+}
+
+resource "aws_s3_bucket" "demo" {
+  bucket = "gitops-demo-${random_id.id.hex}"
+  tags = {
+    Name        = "Terraform GitOps POC"
+    Environment = var.environment
+  }
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.demo.bucket
+}
